c72daeee986756ffe0412be131695e9573b5ea61
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-03-26 09:05:46 -0300

    Fuck off!

--- src/AL1/Domain/TaskItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain;
-
-public class TaskItem
-{
-    public int Id { get; set; }
-    public string Description { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; }
-}
